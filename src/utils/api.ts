697e463cdf971345a82e810bde5ed33f6b3b02d3
Tim <tim040404@mail.ru>
2025-11-24 17:33:24 +0300

    refactor: improve API request logic to reduce code duplication

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,29 +6,31 @@ import type {
 
 export const API_URL = 'https://norma.education-services.ru/api/';
 
-export const fetchIngredientsFromApi = async (): Promise<TIngredient[]> => {
-  const response = await fetch(`${API_URL}ingredients`);
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+const checkResponse = (res: Response): Response => {
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
   }
+  return res;
+};
 
+const request = (endPoint: string, options?: RequestInit): Promise<Response> => {
+  return fetch(`${API_URL}${endPoint}`, options).then(checkResponse);
+};
+
+export const fetchIngredientsFromApi = async (): Promise<TIngredient[]> => {
+  const response = await request('ingredients');
   const ingredientsData = (await response.json()) as TApiResponseFetchIngredients;
   return ingredientsData.data;
 };
 
 export const postOrderToApi = async (ingredientsIds: string[]): Promise<number> => {
-  const response = await fetch(`${API_URL}orders`, {
+  const response = await request('orders', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ ingredients: ingredientsIds }),
   });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
 
   const orderData = (await response.json()) as TApiResponsePostOrder;
   return orderData.order.number;
