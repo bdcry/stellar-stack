5fbb8307f21efc0ed01daefbffdcf20aedc5dbd9
Tim <tim040404@mail.ru>
2025-11-24 18:16:16 +0300

    feat: add loading state to order details modal

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,26 +1,33 @@
 import { useAppSelector } from '@/services/store';
-import { CheckMarkIcon } from '@krgaa/react-developer-burger-ui-components';
+import { CheckMarkIcon, Preloader } from '@krgaa/react-developer-burger-ui-components';
 
 import styles from './order-details.module.css';
 
 const OrderDetails = (): React.JSX.Element => {
   const orderNumber = useAppSelector(({ orderInfo }) => orderInfo.orderNumber);
+  const isLoading = useAppSelector(({ orderInfo }) => orderInfo.status);
 
   return (
     <div className={styles.order_details}>
-      <h2 className={`${styles.order_number} text text_type_digits-large`}>
-        {orderNumber}
-      </h2>
-      <p className={`${styles.order_id} text text_type_main-medium`}>
-        идентификатор заказа
-      </p>
-      <div className={styles.check_icon}>
-        <CheckMarkIcon type="primary" className={styles.check_mark_icon} />
-      </div>
-      <p className="text text_type_main-default">Ваш заказ начали готовить</p>
-      <p className="text text_type_main-default text_color_inactive">
-        Дождитесь готовности на орбитальной станции
-      </p>
+      {isLoading === 'loading' ? (
+        <Preloader />
+      ) : (
+        <>
+          <h2 className={`${styles.order_number} text text_type_digits-large`}>
+            {orderNumber}
+          </h2>
+          <p className={`${styles.order_id} text text_type_main-medium`}>
+            идентификатор заказа
+          </p>
+          <div className={styles.check_icon}>
+            <CheckMarkIcon type="primary" className={styles.check_mark_icon} />
+          </div>
+          <p className="text text_type_main-default">Ваш заказ начали готовить</p>
+          <p className="text text_type_main-default text_color_inactive">
+            Дождитесь готовности на орбитальной станции
+          </p>
+        </>
+      )}
     </div>
   );
 };
