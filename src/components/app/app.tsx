616f3afbc73f5702845c4a5d4ec68d3b7bd7c5b9
Tim <tim040404@mail.ru>
2025-12-14 22:39:00 +0300

    feat: add basic structure of protected routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,10 +6,12 @@ import { Layout } from '../../pages/layout/layout';
 import { Login } from '../../pages/login/login';
 import { Register } from '../../pages/register/register';
 import { ResetPassword } from '../../pages/reset-password/reset-password';
+import { ProtectedRoute } from '../protected-route/protected-route';
 
 import type { JSX } from 'react';
 
 export const App = (): JSX.Element => {
+  const isAuth = false; // позже заменю подтягиванием из стора
   return (
     <BrowserRouter>
       <Routes>
@@ -19,6 +21,10 @@ export const App = (): JSX.Element => {
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
+          <Route element={<ProtectedRoute isAuth={isAuth} />}>
+            <Route path="/profile" element={<div>Профиль</div>} />
+            {/* пока что просто заглушка. компонент и его вложенные пути добавлю позже */}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
